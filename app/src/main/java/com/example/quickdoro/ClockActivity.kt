6b0f82ff4b89deb0aacc79f2a00aa5cf35571531
Michel Melo <michelvmelo@gmail.com>
2023-09-24 04:59:10 -0300

    feat!: add countdown timer

--- app/src/main/java/com/example/quickdoro/ClockActivity.kt
+++ app/src/main/java/com/example/quickdoro/ClockActivity.kt
@@ -1,7 +1,9 @@
 package com.example.quickdoro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.NumberPicker
 
 class ClockActivity : AppCompatActivity() {
@@ -10,13 +12,21 @@ class ClockActivity : AppCompatActivity() {
         setContentView(R.layout.activity_clock)
 
         //Sets max and min values for the number pickers
-        val npMinEstudo = findViewById<NumberPicker>(R.id.np_min_study)
-        val npMinInt = findViewById<NumberPicker>(R.id.np_min_break)
+        val npMinStudy = findViewById<NumberPicker>(R.id.np_min_study)
+        val npMinBreak = findViewById<NumberPicker>(R.id.np_min_break)
 
-        npMinEstudo.minValue = 0
-        npMinEstudo.maxValue = 60
-        npMinInt.minValue = 0
-        npMinInt.maxValue = 60
+        npMinStudy.minValue = 0
+        npMinStudy.maxValue = 60
+        npMinBreak.minValue = 0
+        npMinBreak.maxValue = 60
 
+        //Sets button function to switch to 'Running' activity
+        val startButton = findViewById<Button>(R.id.bt_start)
+        startButton.setOnClickListener{
+            val startRunning = Intent(this, RunningActivity::class.java)
+            startRunning.putExtra("study_min", npMinStudy.value)
+            startRunning.putExtra("break_min", npMinBreak.value)
+            startActivity(startRunning)
+        }
     }
 }
--- app/src/main/java/com/example/quickdoro/RunningActivity.kt
+++ app/src/main/java/com/example/quickdoro/RunningActivity.kt
@@ -2,10 +2,62 @@ package com.example.quickdoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import java.util.Locale
 
 class RunningActivity : AppCompatActivity() {
+
+    private lateinit var studyTimer: CountDownTimer
+    private lateinit var breakTimer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_running)
+
+        val studyMin = intent.getIntExtra("study_min", 0)
+        val breakMin = intent.getIntExtra("break_min", 0)
+
+        val textHype = findViewById<TextView>(R.id.tv_hype)
+        val textClock = findViewById<TextView>(R.id.tv_clock)
+
+        //Countdown timer for study time
+        studyTimer = object : CountDownTimer(studyMin.toLong() * 60 * 1000 + 1000, 1000) {
+            override fun onTick(left: Long) {
+                val mins = (left / 1000) / 60
+                val secs = (left / 1000) % 60
+                textClock.text = String.format(Locale.getDefault(), "%02d:%02d", mins, secs)
+            }
+
+            override fun onFinish() {
+                textHype.text = getText(R.string.relax)
+                breakTimer.start()
+            }
+        }.start()
+
+        //Countdown timer for break time
+        breakTimer = object : CountDownTimer(breakMin.toLong() * 60 * 1000 + 1000, 1000) {
+            override fun onTick(left: Long) {
+                val mins = (left / 1000) / 60
+                val secs = (left / 1000) % 60
+                textClock.text = String.format(Locale.getDefault(), "%02d:%02d", mins, secs)
+            }
+
+            override fun onFinish() {
+                textHype.text = getText(R.string.focus)
+                studyTimer.start()
+            }
+        }
+
+        //Button to finish 'Running' activity
+        val stopButton = findViewById<Button>(R.id.bt_stop)
+        stopButton.setOnClickListener {
+            finish()
+        }
     }
 }
